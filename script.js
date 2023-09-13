3a4225d27a6d2b3e5d5f7baaf1def7a2a67d7cb3
oRenanzinho <rernan.augusto@gmail.com>
2023-09-13 17:03:36 -0300

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+var lista_de_produtos = []
+
+while(true){
+
+    var opcao = prompt(`
+        Bem-Vindo ao cadastro de produtos! :D
+
+        1. Cadastrar novo produto
+        2. Editar produto
+        3. Remover produto
+        4. Visualizar todos os produtos
+    `)
+
+    if(opcao == 1){
+     //código para adicionar produto
+    }else if(opcao == 2){
+        //código para editar produto
+    }else if(opcao == 3){
+        //codigo para remover produto
+    }else if(opcao==4){
+        //codigo para visualizar todos os produtos
+    }else{
+        alert("Nenhuma das opções foi selecionada!")
+    }
+}
